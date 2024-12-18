b10aba1c798ba6b13b1e293e36fd61a2cafb400a
laxmi nishad <laxminishad6387@gmail.com>
2024-12-18 12:02:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/ZigZag Tree Traversal/zig-zag-tree-traversal.java
@@ -0,0 +1,159 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.util.HashMap;
+import java.io.*;
+
+    class Node
+    {
+        int data;
+        Node left,right;
+        Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+    }
+		
+public class GFG2
+{
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    
+	public static void inorder(Node root)
+	{
+	    if(root==null)
+	    return;
+	    inorder(root.left);
+	    System.out.print(root.data);
+	    inorder(root.right);
+	}
+     /* Drier program to test above functions */
+    public static void main(String args[]) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    GFG g = new GFG();
+			
+			    ArrayList<Integer> res = g.zigZagTraversal(root) ;
+			    for (int i = 0; i < res.size (); i++)
+			        System.out.print (res.get (i) + " ");
+			    System. out. println();  
+    			
+                t--;
+            
+        
+System.out.println("~");
+}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*class Node
+{
+    int data;
+    Node left,right;
+    Node(int d)
+    {
+        data=d;
+        left=right=null;
+    }
+}*/
+
+class GFG
+{
+    ArrayList<Integer> result=new ArrayList<>();
+	ArrayList<Integer> zigZagTraversal(Node root)
+	{
+	    boolean flag=true;
+	    Deque<Node> dq=new ArrayDeque<>();
+	    if(root==null)
+	      return result;
+	      dq.add(root);
+	      while(!dq.isEmpty())
+	      {
+	         int level=dq.size();
+	         ArrayList<Integer> ans=new ArrayList<>();
+	         for(int i=0; i<level; i++)
+	         {
+	             Node ele=dq.pollFirst();
+	             ans.add(ele.data);
+	             if(ele.left!=null)
+	               dq.addLast(ele.left);
+	               if(ele.right!=null)
+	               dq.addLast(ele.right);
+	               
+	         }
+	         if(!flag)
+	         {
+	             Collections.reverse(ans);
+	         }
+	         result.addAll(ans);
+	         flag=!flag;
+	      }
+	      return result;
+	}
+}
