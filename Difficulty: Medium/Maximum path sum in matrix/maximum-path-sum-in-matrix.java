92b599b00c333c1e178948bb246e808e75957636
laxmi nishad <laxminishad6387@gmail.com>
2025-01-31 22:03:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum path sum in matrix/maximum-path-sum-in-matrix.java
+++ Difficulty: Medium/Maximum path sum in matrix/maximum-path-sum-in-matrix.java
@@ -1,63 +1,71 @@
 //{ Driver Code Starts
-//Initial Template for Java
-
-import java.io.*;
+// Initial Template for Java
 import java.util.*;
 
-class GFG
-{
-    public static void main(String args[])throws IOException
-    {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(in.readLine());
-        while(t-- > 0){
-            int N = Integer.parseInt(in.readLine());
-            String input_line[] = in.readLine().trim().split("\\s+");
-            int Matrix[][] = new int[N][N];
-            for(int i = 0; i < N*N; i++)
-                Matrix[(i/N)][i%N] = Integer.parseInt(input_line[i]);
-            
+class gfg {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt(), m = sc.nextInt();
+            int[][] mat = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    mat[i][j] = sc.nextInt();
+                }
+            }
             Solution ob = new Solution();
-            System.out.println(ob.maximumPath(N, Matrix));
-        
-System.out.println("~");
-}
+            System.out.println(ob.maximumPath(mat));
+            System.out.println("~");
+        }
     }
 }
 
 // } Driver Code Ends
 
 
-//User function Template for Java
-// solve using dp problem
-class Solution{
-    static int maximumPath(int n, int mat[][])
-    {
-        // we use previous value in current position
-        if(n==1)
-        return mat[0][0];
-       for(int i=1; i<n; i++)
-       {
-          for(int j=0; j<n; j++)
-          {
-             if (j == 0) {
-                 
-                    mat[i][j] += Math.max(mat[i - 1][j], mat[i - 1][j + 1]);
-                } else if (j == n - 1) {
-                   
-                    mat[i][j] += Math.max(mat[i - 1][j], mat[i - 1][j - 1]);
-                } 
-                else
-              mat[i][j]+=Math.max(mat[i-1][j],Math.max(mat[i-1][j-1],mat[i-1][j+1]));
+class Solution {
+    public int maximumPath(int[][] arr) {
+     int m=arr.length; //number of rows
+     int n=arr[0].length; // number of columns
+     int ans[][]=new int[m][n];  //create another matrix
+     for(int i=0; i<m; i++)
+     {
+         for(int j=0; j<n; j++)
+          ans[i][j]=arr[i][j];
+     }
+     for(int i=0; i<m; i++)
+     {
+        for(int j=0; j<n; j++)
+        {
+            if(j>0  && i<m-1)  //handle left diagonal //arr[i+1][j-1]
+            {
+                int sum=ans[i][j]+arr[i+1][j-1];
+                if(sum>ans[i+1][j-1])
+                  ans[i+1][j-1]=sum;
+            }
+           
+            if(i<m-1)//handle down pointer  //arr[i+1][j]
+            {
+                int sum=ans[i][j]+arr[i+1][j];
+                if(sum>ans[i+1][j])
+                  ans[i+1][j]=sum;
+            }
+            if(j<n-1 && i<m-1)//handle right diagonal //arr[i+1][j+1]
+            {
+                int sum=ans[i][j]+arr[i+1][j+1];
+                if(sum>ans[i+1][j+1])
+                  ans[i+1][j+1]=sum;
+            }
             
-          }
-       }
-       int maxValue=0;
-     
-       for(int j=0; j<n; j++)
-       {
-         maxValue=Math.max(mat[n-1][j],maxValue);
-       }
-       return maxValue;
+        }
+     }
+     int max=ans[m-1][0];
+      for(int i=1; i<n; i++)
+      {
+          if(ans[m-1][i]>max)
+         max=ans[m-1][i];
+      }
+      return max;
     }
 }
