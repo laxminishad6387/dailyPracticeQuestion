a9a562b4d7b402faec3b9913dc36246d0bf2ea58
laxmi nishad <laxminishad6387@gmail.com>
2025-02-19 08:59:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while (t-- > 0) {
+            int W = Integer.parseInt(in.readLine().trim());
+            String line2[] = in.readLine().trim().split(" ");
+            int N = line2.length;
+            int val[] = new int[N];
+            for (int i = 0; i < N; i++) val[i] = Integer.parseInt(line2[i]);
+            String line3[] = in.readLine().trim().split(" ");
+            int wt[] = new int[N];
+            for (int i = 0; i < N; i++) wt[i] = Integer.parseInt(line3[i]);
+
+            Solution ob = new Solution();
+            System.out.println(ob.knapSack(val, wt, W));
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static int knapSack(int val[], int wt[], int capacity) {
+      int dp[] = new int[capacity + 1];
+
+        for (int i = 0; i <= capacity; i++) {
+            for (int j = 0; j < val.length; j++) {
+                if (wt[j] <= i) {  // If item can be included
+                    dp[i] = Math.max(dp[i], dp[i - wt[j]] + val[j]);
+                }
+            }
+        }
+        return dp[capacity];
+    }
+    
+}
