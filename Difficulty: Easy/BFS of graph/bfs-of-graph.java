76d86bec1e2093ab525665efc5288d7f6a1da287
laxmi nishad <laxminishad6387@gmail.com>
2025-06-11 03:20:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.java
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.java
@@ -1,12 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for Java
-import java.io.*;
-import java.util.*;
-
-
-// } Driver Code Ends
-
-// User function Template for Java
 class Solution {
     // Function to return Breadth First Search Traversal of given graph.
     public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
@@ -16,11 +7,10 @@ class Solution {
        q.add(0);
        while(!q.isEmpty()){
            int currNode=q.remove();
-           if(!vis[currNode]){
+           if(vis[currNode]==false){
                vis[currNode]=true;
                result.add(currNode);
-               for(int i=0; i<adj.get(currNode).size(); i++)
-               {
+               for(int i=0; i<adj.get(currNode).size(); i++){
                    q.add(adj.get(currNode).get(i));
                }
            }
@@ -29,39 +19,3 @@ class Solution {
         
     }
 }
-
-
-//{ Driver Code Starts.
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine().trim());
-
-        while (tc-- > 0) {
-            int V = Integer.parseInt(br.readLine().trim());
-            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-
-            for (int i = 0; i < V; i++) {
-                String[] input = br.readLine().trim().split(" ");
-                ArrayList<Integer> node = new ArrayList<>();
-                for (String s : input) {
-                    if (!s.isEmpty()) {
-                        node.add(Integer.parseInt(s));
-                    }
-                }
-                adj.add(node);
-            }
-
-            Solution obj = new Solution();
-            ArrayList<Integer> ans = obj.bfs(adj);
-            for (int num : ans) {
-                System.out.print(num + " ");
-            }
-            System.out.println();
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
